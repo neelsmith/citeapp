d83c799378dddc269763de9ebcf7c559adc357c5
Neel Smith <dnsmith.neel@gmail.com>
2017-01-29 05:58:44 -0500

    Add skeleton

---
+++ src/test/scala/example/CiteAppTest.scala
@@ -0,0 +1,16 @@
+package edu.holycross.shot
+
+import utest._
+
+object CiteApp extends TestSuite {
+
+  import CiteApp._
+
+  def tests = TestSuite {
+    'ScalaJSExample {
+      assert(square(0) == 0)
+      assert(square(4) == 16)
+      assert(square(-5) == 25)
+    }
+  }
+}
