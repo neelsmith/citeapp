d75b889941c9c19c5ada37a1f2b370f61fd7da6c
Neel Smith <dnsmith.neel@gmail.com>
2017-01-29 06:05:17 -0500

    Rename directories

--- src/test/scala/citeapp/CiteAppTest.scala
+++ src/test/scala/citeapp/CiteAppTest.scala
@@ -7,7 +7,7 @@ object CiteApp extends TestSuite {
   import CiteApp._
 
   def tests = TestSuite {
-    'ScalaJSExample {
+    'CiteApp {
       assert(square(0) == 0)
       assert(square(4) == 16)
       assert(square(-5) == 25)
